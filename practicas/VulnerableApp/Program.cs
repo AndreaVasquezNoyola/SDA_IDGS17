acb30ba5fda09baf3dd528a059fec53679579e21
AndreaVasquez <andyy_vasquez_2102@hotmail.com>
2026-06-02 08:09:48 -0600

    avance practica 3

--- practicas/VulnerableApp/Program.cs
+++ practicas/VulnerableApp/Program.cs
@@ -5,9 +5,10 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllersWithViews();
 
-
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddSession();
 
 var app = builder.Build();
 
@@ -19,6 +20,8 @@ if (!app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 app.UseRouting();
+
+app.UseSession();
 
 app.UseAuthorization();
 
